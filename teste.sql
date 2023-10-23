f3d2b3750c6314b0751b10c20a8c48dfff7f7f70
Luan Masson <111405027+Luan-Masson@users.noreply.github.com>
2023-10-23 09:33:36 -0300

    adicionando arquivo sql básico para rodar o projeto

---
+++ teste.sql
@@ -0,0 +1,90 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 23/10/2023 às 14:28
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `teste`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `cidade`
+--
+
+CREATE TABLE `cidade` (
+  `cidID` int(11) NOT NULL,
+  `cidNome` varchar(255) NOT NULL,
+  `cidCep` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuariosadm`
+--
+
+CREATE TABLE `usuariosadm` (
+  `usuID` int(11) NOT NULL,
+  `usuNome` varchar(255) NOT NULL,
+  `usuSenha` char(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `usuariosadm`
+--
+
+INSERT INTO `usuariosadm` (`usuID`, `usuNome`, `usuSenha`) VALUES
+(1, 'luan', '7c4a8d09ca3762af61e59520943dc26494f8941b');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `cidade`
+--
+ALTER TABLE `cidade`
+  ADD PRIMARY KEY (`cidID`);
+
+--
+-- Índices de tabela `usuariosadm`
+--
+ALTER TABLE `usuariosadm`
+  ADD PRIMARY KEY (`usuID`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `cidade`
+--
+ALTER TABLE `cidade`
+  MODIFY `cidID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `usuariosadm`
+--
+ALTER TABLE `usuariosadm`
+  MODIFY `usuID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
